7c4b49fef771796e2312182521db5015b6429955
iamzh <iamzh@DESKTOP-7JLVQQU>
2021-11-19 19:03:35 +0500

    Добавьте файлы проекта.

---
+++ pr-32_prakt17_1_OAP_ZhuravlevTimofeyVasilievich/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace pr_32_prakt17_1_OAP_ZhuravlevTimofeyVasilievich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] k = Enumerable.Range(0, 10).Select(i =>
+            {
+                Console.Write($"k[{i}] = ");
+                return double.Parse(Console.ReadLine());
+            }).ToArray();
+            double sum = k.Sum();
+            Console.WriteLine($"Сумма чисел = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
